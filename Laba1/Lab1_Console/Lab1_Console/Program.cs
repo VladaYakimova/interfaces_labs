de55465da49fd512c96baff766c763182537b8e9
vl-os <vld.osipenko@gmail.com>
2019-05-19 14:07:28 +0300

    init commit

---
+++ Laba1/Lab1_Console/Lab1_Console/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+using SimpleJSON;
+using System.Collections.Specialized;
+using Telegram.Bot;
+using System.Text.RegularExpressions;
+
+namespace Lab1_Console
+{
+    class Program
+    {
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("883601079:AAG6P60xi1Ycde9tDYE-nr8fg7-H-yJ4wKg");
+        private static Dictionary<string, string> users = new Dictionary<string, string>();
+        private static readonly Regex regID = new Regex(@"\d+");
+        private static readonly Regex regName = new Regex(@"!\d+");
+        static void Main(string[] args)
+        {        
+            Console.WriteLine("Отправка и получение сообщений. Бот: @VladaYakimovaBot ");
+
+            Bot.OnMessage += Bot_OnMessage;
+            string message = "";
+            string userID = "";
+            string command;
+            bool exception = false;
+            while (true)
+            {
+                Console.WriteLine("Для отправки сообщений введите - 1");
+                Console.WriteLine("Для просмотра полученых сообщений введите - 2");
+            
+                Console.Write("$");
+                command = Console.ReadLine();
+                switch (command)
+                {
+                    case "2":
+                        Console.WriteLine("Полученные сообщения:"+"\n"+"(для выхода нажмите любую клавишу)" + "\n");
+                        Bot.StartReceiving();
+                        Console.ReadKey();
+                        Bot.StopReceiving();                     
+                        break;
+                    case "1":
+                        do
+                        {                         
+                            foreach (KeyValuePair<string, string> keyValue in users)
+                            {
+                                Console.WriteLine(keyValue.Key + " - " + keyValue.Value);
+                            }
+
+                            Console.Write('\n' + "ID пользователя, которому вы хотите отправит сообщение:" + "\n" + "(чтобы отправить сообщение администратору введите - a)" + "\n" + '\n');
+                            userID = Console.ReadLine();
+                            Console.Write('\n' + "Напишите сообщение" + '\n' + ">");
+                            message = Console.ReadLine();
+                            if(userID=="a")
+                            Bot.SendTextMessageAsync("vladavh", message);
+                            Bot.SendTextMessageAsync(userID, message);
+                            exception = false;
+                        }
+                        while (exception);
+                        break;
+                    default:
+                        Console.Write('\n' + "Неверная команда");
+                        break;
+                }
+                Console.Write('\n' + "_________________________________________________________" + '\n');
+            }
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        {
+            if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
+            {
+                try
+                {
+                    users.Add(e.Message.From.ToString().Substring(0, e.Message.From.ToString().IndexOf(' ')), regID.Match(e.Message.From.ToString()).ToString());
+                }
+                catch (ArgumentException) { }
+                if (e.Message.IsForwarded)
+                {
+                    try
+                    {
+                        users.Add(e.Message.ForwardFrom.ToString().Substring(0, e.Message.From.ToString().IndexOf(' ')), regID.Match(e.Message.ForwardFrom.ToString()).ToString());
+                    }
+                    catch (ArgumentException) { }
+                    Console.Write("\n" + "Переслано от: " + e.Message.ForwardFrom.ToString().Substring(0, e.Message.From.ToString().IndexOf(' ')));
+                }
+                    Console.Write("\n" + e.Message.From.ToString().Substring(0, e.Message.From.ToString().IndexOf(' ')) + ": ");
+                    Console.Write(e.Message.Text + "  ");
+                    Console.Write(e.Message.Date.ToLocalTime()+"\n");
+            }
+            else {
+                Console.WriteLine("Получен файл"); }
+        }
+    }
+}
---
+++ Laba2/Kalkulyator/Kalkulyator/Form1.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kalkulyator
+{
+    public partial class Калькулятор : Form
+    {
+        double dija = 0;
+        string old;
+        double vidp;
+        public Калькулятор()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button14_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "1";
+        }
+
+        private void button15_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "2";
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "3";
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "4";
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "5";
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "6";
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "7";
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "8";
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "9";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += ",";
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text += "0";
+        }
+
+        private void button17_Click(object sender, EventArgs e)
+        {
+            dija = 1;
+            old = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button18_Click(object sender, EventArgs e)
+        {
+            dija = 2;
+            old = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button19_Click(object sender, EventArgs e)
+        {
+            dija = 3;
+            old = textBox1.Text;
+            textBox1.Text = "";
+        }
+
+        private void button13_Click(object sender, EventArgs e)
+        {
+            dija = 4;
+            old = textBox1.Text;
+            textBox1.Text = "";
+        }
+        
+        private void button20_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            double vidp1 = Math.Sqrt(Convert.ToDouble(textBox1.Text));
+            textBox1.Text = Convert.ToString(vidp1);
+        }
+        private void button22_Click(object sender, EventArgs e)
+        {
+            dija = 5;
+            old = textBox1.Text;
+            textBox1.Text = "";
+        }
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (dija == 1)
+            {
+                vidp = (Convert.ToDouble(old) + Convert.ToDouble(textBox1.Text));
+                textBox1.Text = Convert.ToString(vidp);
+            }
+
+            if (dija == 2)
+            {
+                vidp = (Convert.ToDouble(old) - Convert.ToDouble(textBox1.Text));
+                textBox1.Text = Convert.ToString(vidp);
+            }
+
+            if (dija == 3)
+            {
+                try
+                {
+                    vidp = (Convert.ToDouble(old) / Convert.ToDouble(textBox1.Text));
+                    textBox1.Text = Convert.ToString(vidp);
+                } catch 
+                {
+                    textBox1.Text = "";
+                }
+            }
+
+            if (dija == 4)
+            {
+                vidp = (Convert.ToDouble(old) * Convert.ToDouble(textBox1.Text));
+                textBox1.Text = Convert.ToString(vidp);
+            }
+
+            if (dija == 5)
+            {
+                vidp = Math.Pow(Convert.ToDouble(old), Convert.ToDouble(textBox1.Text));
+                textBox1.Text = Convert.ToString(vidp);
+            }
+
+            
+        }
+
+
+    }
+}
